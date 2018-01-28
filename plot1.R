55b2573e6e6ccd4ef3bdd5df38367e6c138ae521
jmscarrillo <jmscarrillo@gmail.com>
2018-01-28 18:20:14 +0100

    Initial commit.

---
+++ plot1.R
@@ -0,0 +1,8 @@
+## Coursera - Data Science - Universidad Johns Hopkins
+## Exploratory Data Analysis - Week 4
+## Question 1:
+##  Have total emissions from PM2.5 decreased in the United States from 1999 to
+##  2008? Using the base plotting system, make a plot showing the total PM2.5
+##  emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+## José Mª Sebastián Carrillo
+
